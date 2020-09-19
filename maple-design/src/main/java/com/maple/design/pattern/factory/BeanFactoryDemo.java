e75942de5ffef52edcfb75351604e8a28e460f20
sakura-maple <435302451@qq.com>
2020-09-19 10:46:21 +0800

    添加相关依赖

---
+++ maple-design/src/main/java/com/maple/design/pattern/factory/BeanFactoryDemo.java
@@ -0,0 +1,8 @@
+package com.maple.design.pattern.factory;
+
+import org.springframework.beans.factory.BeanFactory;
+
+public class BeanFactoryDemo {
+    public static void main(String[] args) {
+    }
+}
